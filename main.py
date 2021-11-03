bd55d33db05a7f625c4b900a180fec53ddb45d88
sanggusti <gustiowinata0@gmail.com>
2021-11-03 19:07:25 +0700

    Setting up auto reload for celery to ease development

--- main.py
+++ main.py
@@ -3,3 +3,17 @@ from project import create_app
 
 app = create_app()
 celery = app.celery_app
+
+
+def celery_worker():
+    from watchgod import run_process
+    import subprocess
+
+    def run_worker():
+        subprocess.call(["celery", "-A", "main.celery", "worker", "--loglevel=info"])
+
+    run_process("./project", run_worker)
+
+
+if __name__ == "__main__":
+    celery_worker()
