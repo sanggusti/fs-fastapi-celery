fc933c95f92368b807097460ea3b56642c60f8b7
sanggusti <gustiowinata0@gmail.com>
2021-12-02 11:06:28 +0700

    Adding sampe view and task for subscribtion
    initiate pytest configuration

--- project/users/views.py
+++ project/users/views.py
@@ -11,7 +11,12 @@ from sqlalchemy.orm import Session
 
 from . import users_router
 from .schemas import UserBody
-from .tasks import sample_task, task_process_notification, task_send_welcome_email
+from .tasks import (
+    sample_task,
+    task_process_notification,
+    task_send_welcome_email,
+    task_add_subscribe,
+)
 from .models import User
 from project.database import get_db_session
 
@@ -106,3 +111,28 @@ def transaction_celery(session: Session = Depends(get_db_session)):
     logger.info(f"user {user.id} {user.username} is persistent now")
     task_send_welcome_email.delay(user.id)
     return {"message": "done"}
+
+
+@users_router.post("/user_subscribe/")
+def user_subscribe(
+    user_body: UserBody,
+    session: Session = Depends(get_db_session),
+):
+    try:
+        user = session.query(User).filter_by(username=user_body.username).first()
+        if user:
+            user_id = user.id
+        else:
+            user = User(
+                username=user_body.username,
+                email=user_body.email,
+            )
+            session.add(user)
+            session.commit()
+            user_id = user.id
+    except Exception as e:
+        session.rollback()
+        raise
+
+    task_add_subscribe.delay(user_id)
+    return {"message": "send task to Celery successfully"}
