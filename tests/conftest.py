4b0f93b79b06e53f808b7aceaaed1ac8e5c80677
sanggusti <gustiowinata0@gmail.com>
2022-01-02 08:41:41 +0700

    ➕Add custom fixture for running files in temp directory

--- tests/conftest.py
+++ tests/conftest.py
@@ -44,3 +44,8 @@ def client(app):
     from fastapi.testclient import TestClient
 
     yield TestClient(app)
+
+
+@pytest.fixture(autouse=True)
+def tmp_upload_dir(tmpdir, settings):
+    settings.UPLOADS_DEFAULT_DEST = tmpdir.mkdir("tmp")
